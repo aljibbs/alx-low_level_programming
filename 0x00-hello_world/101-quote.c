0a2153f3959b08a2abc170abeff209820acd0537
aljibbs <aljibbs.aj@gmail.com>
2023-03-17 21:49:07 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (0);
 }
