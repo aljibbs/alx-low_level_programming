c062bc9a7b89e685923e55232807fe095217f550
aljibbs <aljibbs.aj@gmail.com>
2023-03-17 23:53:07 -0700

    alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,18 +2,16 @@
 /* more headers goes there */
 /**
  * main - Main function
- * return - 0
+ * Return: 0
  */
 /* betty style doc for function main goes there */
+
 int main(void)
 {
-	//int n;
-
-	//srand(time(0));
-	//n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	char c;
-	for(c = 'a'; c <= 'z'; c++)
+
+	for (c = 'a'; c <= 'z'; c++)
 	putchar(c);
 	putchar('\n');
 	return (0);
