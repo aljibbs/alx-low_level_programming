a5426ba87f2f226df3b6af956a7af1b53cfec0e9
aljibbs <aljibbs.aj@gmail.com>
2023-03-17 22:59:08 -0700

    removed trailing space

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,15 +4,28 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * Always return zero (Success)
+ * main - function
+ * Return: Always (Success)
+ */
 int main(void)
 {
 int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-if(n > 0){ printf("%d is positive\n", n);}
-else if(n < 0){printf("%d is negative\n", n);}
-else{printf("%d is zero\n", n);}
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else
+{
+printf("%d is zero\n", n);
+}
+return (0);
 }
