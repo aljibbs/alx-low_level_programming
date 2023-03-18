caf6b24f8ea988c022ff66e00f0d581ee3055eea
aljibbs <aljibbs.aj@gmail.com>
2023-03-18 07:02:36 -0700

    Printing alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* more headers goes there */
+/**
+ * main - Main function
+ * Return: Success
+ */
+/* betty style doc for function main goes there */
+
+int main(void)
+{
+	char val, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (val = 'a'; val <= 'z'; val++)
+	{
+		if (val != q && val != q)
+		{
+			putchar(val);
+			putchar('\n');
+		}
+	}
+}
