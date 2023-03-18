1bbce64fedea7b9837877555eb859882e9fce5c5
aljibbs <aljibbs.aj@gmail.com>
2023-03-18 08:02:54 -0700

    Fixes

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -21,12 +21,10 @@ int main(void)
 				putchar(p);
 
 				if (d == '8' && p == '9')
-				{
+				
 					continue;
-					putchar(',');
-					putchar(' ');
-				}
-
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
