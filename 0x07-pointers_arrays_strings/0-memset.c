d3b71ee74bdf43d8003ce61b70a52a40dff172ed
aljibbs <aljibbs.aj@gmail.com>
2023-04-03 11:15:53 -0700

    Minor changes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,10 +11,12 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
-	for (; n > 0; i++)
+	/* Loop through */
+	for (; i < n; i++)
 	{
 		s[i] = b;
-		n--;
 	}
+
+	/* return the value of s */
 	return (s);
 }
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,13 +9,23 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	/* declare variables */
+	/* variable r */
 	int r = 0;
+
+	/* variable i */
 	int i = n;
 
+	/* Loop through */
 	for (; r < i; r++)
 	{
+		/* replace value of position r in dest with value of position r in src */
 		dest[r] = src[r];
+
+		/* reduce n by 1 */
 		n--;
 	}
+
+	/* return dest */
 	return (dest);
 }
